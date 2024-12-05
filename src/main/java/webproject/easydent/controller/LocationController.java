dd95e1ed527c0c09f4d593079a56a25b12689f91
GameCod1ng <hyeongseok36@naver.com>
2024-12-05 17:17:31 +0900

    12.05 3차

--- src/main/java/webproject/easydent/controller/LocationController.java
+++ src/main/java/webproject/easydent/controller/LocationController.java
@@ -2,6 +2,7 @@ package webproject.easydent.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,20 +25,41 @@ public class LocationController {
     public final LocationRepository locationRepository;
 
 
-    @CrossOrigin(origins = "http://localhost:9090", allowCredentials = "true")
-//    @CrossOrigin(origins = "http://192.168.45.179:9090", allowCredentials = "true")
+    //    @CrossOrigin(origins = "http://localhost:9090", allowCredentials = "true")
+////    @CrossOrigin(origins = "http://192.168.45.179:9090", allowCredentials = "true")
+//    @PostMapping("/home/location")
+////    @ResponseBody
+//    public String saveLocation(Model model, @RequestBody Location location, @AuthenticationPrincipal CustomOAuth2User customOAuth2User){
+//        log.info(location.bname);
+//        if (customOAuth2User != null) {
+//            String email = customOAuth2User.getUser().getEmail();
+//            User user = userService.findByEmail(email);
+//            location.setUser(user);
+//            model.addAttribute("bname", location.bname);
+//            locationService.saveLocation(location);
+//        }
+//        return "redirect:/home";
+//    }
     @PostMapping("/home/location")
     @ResponseBody
-    public Location saveLocation(@RequestBody Location location, @AuthenticationPrincipal CustomOAuth2User customOAuth2User){
-        log.info(location.bname);
+    public ResponseEntity<?> saveLocation(@RequestBody Location location, @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
         if (customOAuth2User != null) {
             String email = customOAuth2User.getUser().getEmail();
             User user = userService.findByEmail(email);
             location.setUser(user);
+            locationService.saveLocation(location);
+            return ResponseEntity.ok(location);
         }
-        return locationService.saveLocation(location);
+        return ResponseEntity.badRequest().build();
     }
 
-
+    @GetMapping("/user/current")
+    @ResponseBody
+    public String getCurrentUserEmail(@AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+        if (customOAuth2User != null) {
+            return customOAuth2User.getUser().getEmail();
+        }
+        return "";
+    }
 }
 
